57872858bb978b1a85b860db09c7b928d2e4fe18
Jacob Bartlett <jacob@lapse.com>
2025-04-17 09:59:18 +0100

    Initial Commit

---
+++ CombineWebsockets/CombineWebsocketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineWebsocketsApp.swift
+//  CombineWebsockets
+//
+//  Created by Jacob Bartlett on 17/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CombineWebsocketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
